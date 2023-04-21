cd5ad5f462f7371d2a2bdd709475344e67be3bc3
echo music <php_fangting@126.com>
2023-04-21 17:43:44 +0800

    env

--- app/user/cmd/user/main.go
+++ app/user/cmd/user/main.go
@@ -21,7 +21,7 @@ import (
 // go build -ldflags "-X main.Version=x.y.z"
 var (
 	// Name is the name of the compiled software.
-	Name string = "helloword"
+	Name string = "user"
 	// Version is the version of the compiled software.
 	Version string
 	// flagconf is the config flag.
